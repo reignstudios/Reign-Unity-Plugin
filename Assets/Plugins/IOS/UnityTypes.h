64e7fd3904854af3114cb87a0fe665b5b4314e3d
Andrew Witte <zezba9000@gmail.com>
2014-11-24 16:44:35 -0800

    Made Unity4.3.4 support desc more easy to understand.

--- Assets/Plugins/IOS/UnityTypes.h
+++ Assets/Plugins/IOS/UnityTypes.h
@@ -6,8 +6,8 @@
 //void UnityPause(bool pause);// No longer supported in Unity 4.3+
 //void UnitySendMessage(const char * className, const char * methodName, const char * param);
 
-//UIViewController *UnityGetGLViewController();// Not supported in Unity 4.5+
-extern "C" UIViewController *UnityGetGLViewController();
+//UIViewController *UnityGetGLViewController();// Use for Unity4.3.4- support.
+extern "C" UIViewController *UnityGetGLViewController();// Use for Unity4.5+ support.
 
 // Converts C style string to NSString
 #define GetStringParam( _x_ ) ( _x_ != NULL ) ? [NSString stringWithUTF8String:_x_] : [NSString stringWithUTF8String:""]
