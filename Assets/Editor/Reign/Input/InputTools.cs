72dc684ea46d271a25283174ce5b1d87e9fda867
Andrew <zezba9000@gmail.com>
2015-01-30 01:28:13 -0800

    InputEx experiments looking good.

--- Assets/Editor/Reign/Input/InputTools.cs
+++ Assets/Editor/Reign/Input/InputTools.cs
@@ -7,6 +7,18 @@ namespace Reign.EditorTools
 {
 	public class InputTools
 	{
+		[MenuItem("Edit/Reign/Input/Save Universal Default")]
+		static void SaveAsUniversalDefaultLayout()
+		{
+			saveActiveLayoutAsDefault("Universal");
+		}
+
+		[MenuItem("Edit/Reign/Input/Apply Universal Default")]
+		static void ApplyUniversalDefaultInputLayout()
+		{
+			applyDefaultLayout("Universal");
+		}
+
 		[MenuItem("Edit/Reign/Input/Save active platform layout as Default")]
 		static void SaveAsDefaultLayout()
 		{
@@ -15,6 +27,72 @@ namespace Reign.EditorTools
 				case BuildTarget.StandaloneWindows:
 				case BuildTarget.StandaloneWindows64:
 					saveActiveLayoutAsDefault("Win32");
+					break;
+
+				case BuildTarget.StandaloneOSXIntel:
+				case BuildTarget.StandaloneOSXIntel64:
+				case BuildTarget.StandaloneOSXUniversal:
+					saveActiveLayoutAsDefault("OSX");
+					break;
+
+				case BuildTarget.StandaloneLinux:
+				case BuildTarget.StandaloneLinux64:
+				case BuildTarget.StandaloneLinuxUniversal:
+					saveActiveLayoutAsDefault("Linux");
+					break;
+
+				case BuildTarget.MetroPlayer:
+					saveActiveLayoutAsDefault("WinRT");
+					break;
+
+				case BuildTarget.WP8Player:
+					saveActiveLayoutAsDefault("WP8");
+					break;
+
+				case BuildTarget.iPhone:
+					saveActiveLayoutAsDefault("iOS");
+					break;
+
+				case BuildTarget.Android:
+					saveActiveLayoutAsDefault("Android");
+					break;
+
+				case BuildTarget.BlackBerry:
+					saveActiveLayoutAsDefault("BlackBerry");
+					break;
+
+				case BuildTarget.Tizen:
+					saveActiveLayoutAsDefault("Tizen");
+					break;
+
+				case BuildTarget.SamsungTV:
+					saveActiveLayoutAsDefault("SamsungTV");
+					break;
+
+				case BuildTarget.WebPlayer:
+				case BuildTarget.WebPlayerStreamed:
+					saveActiveLayoutAsDefault("WebPlayer");
+					break;
+
+				case BuildTarget.PSM:
+				case BuildTarget.PSP2:
+					saveActiveLayoutAsDefault("Vita");
+					break;
+
+				case BuildTarget.PS3:
+					saveActiveLayoutAsDefault("PS3");
+					break;
+
+				case BuildTarget.PS4:
+					saveActiveLayoutAsDefault("PS4");
+					break;
+
+				case BuildTarget.XBOX360:
+					saveActiveLayoutAsDefault("Xbox360");
+					break;
+
+				case BuildTarget.XboxOne:
+					saveActiveLayoutAsDefault("XboxOne");
 					break;
 
 				default: Debug.LogError("Unsuported platform: " + EditorUserBuildSettings.activeBuildTarget); break;
@@ -31,8 +109,98 @@ namespace Reign.EditorTools
 					applyDefaultLayout("Win32");
 					break;
 
+				case BuildTarget.StandaloneOSXIntel:
+				case BuildTarget.StandaloneOSXIntel64:
+				case BuildTarget.StandaloneOSXUniversal:
+					applyDefaultLayout("OSX");
+					break;
+
+				case BuildTarget.StandaloneLinux:
+				case BuildTarget.StandaloneLinux64:
+				case BuildTarget.StandaloneLinuxUniversal:
+					applyDefaultLayout("Linux");
+					break;
+
+				case BuildTarget.MetroPlayer:
+					applyDefaultLayout("WinRT");
+					break;
+
+				case BuildTarget.WP8Player:
+					applyDefaultLayout("WP8");
+					break;
+
+				case BuildTarget.iPhone:
+					applyDefaultLayout("iOS");
+					break;
+
+				case BuildTarget.Android:
+					applyDefaultLayout("Android");
+					break;
+
+				case BuildTarget.BlackBerry:
+					applyDefaultLayout("BlackBerry");
+					break;
+
+				case BuildTarget.Tizen:
+					applyDefaultLayout("Tizen");
+					break;
+
+				case BuildTarget.SamsungTV:
+					applyDefaultLayout("SamsungTV");
+					break;
+
+				case BuildTarget.WebPlayer:
+				case BuildTarget.WebPlayerStreamed:
+					applyDefaultLayout("WebPlayer");
+					break;
+
+				case BuildTarget.PSM:
+				case BuildTarget.PSP2:
+					applyDefaultLayout("Vita");
+					break;
+
+				case BuildTarget.PS3:
+					applyDefaultLayout("PS3");
+					break;
+
+				case BuildTarget.PS4:
+					applyDefaultLayout("PS4");
+					break;
+
+				case BuildTarget.XBOX360:
+					applyDefaultLayout("Xbox360");
+					break;
+
+				case BuildTarget.XboxOne:
+					applyDefaultLayout("XboxOne");
+					break;
+
 				default: Debug.LogError("Unsuported platform: " + EditorUserBuildSettings.activeBuildTarget); break;
 			}
+		}
+
+		[MenuItem("Edit/Reign/Input/Save custom platform layout")]
+		static void SaveCustomPlatformLayout()
+		{
+			string filename = EditorUtility.SaveFilePanel("Input Layout", Application.dataPath+"", "InputManager_Custom", "");
+			if (string.IsNullOrEmpty(filename)) return;
+			
+			EditorApplication.SaveAssets();
+			string root = Application.dataPath.Replace("Assets", "ProjectSettings");
+			File.Copy(root + "/InputManager.asset", filename, true);
+			AssetDatabase.Refresh();
+		}
+
+		[MenuItem("Edit/Reign/Input/Load custom platform layout")]
+		static void LoadCustomPlatformLayout()
+		{
+			string filename = EditorUtility.OpenFilePanel("Input Layout", Application.dataPath+"", "");
+			if (string.IsNullOrEmpty(filename)) return;
+			
+			EditorApplication.SaveAssets();
+			string root = Application.dataPath.Replace("Assets", "ProjectSettings");
+			File.Copy(filename, root + "/InputManager.asset", true);
+			AssetDatabase.Refresh();
 		}
 
 		static void saveActiveLayoutAsDefault(string platform)
--- Assets/Plugins/Reign/Shared/Code/InputEx.cs
+++ Assets/Plugins/Reign/Shared/Code/InputEx.cs
@@ -3,255 +3,19 @@ using System.Collections;
 
 namespace Reign
 {
-	public enum KeyCodeEx
+	public enum Buttons
 	{
-		None = 0,
-		Backspace = 8,
-		Tab = 9,
-		Clear = 12,
-		Return = 13,
-		Pause = 19,
-		Escape = 27,
-		Space = 32,
-		Exclaim = 33,
-		DoubleQuote = 34,
-		Hash = 35,
-		Dollar = 36,
-		Ampersand = 38,
-		Quote = 39,
-		LeftParen = 40,
-		RightParen = 41,
-		Asterisk = 42,
-		Plus = 43,
-		Comma = 44,
-		Minus = 45,
-		Period = 46,
-		Slash = 47,
-		Alpha0 = 48,
-		Alpha1 = 49,
-		Alpha2 = 50,
-		Alpha3 = 51,
-		Alpha4 = 52,
-		Alpha5 = 53,
-		Alpha6 = 54,
-		Alpha7 = 55,
-		Alpha8 = 56,
-		Alpha9 = 57,
-		Colon = 58,
-		Semicolon = 59,
-		Less = 60,
-		Equals = 61,
-		Greater = 62,
-		Question = 63,
-		At = 64,
-		LeftBracket = 91,
-		Backslash = 92,
-		RightBracket = 93,
-		Caret = 94,
-		Underscore = 95,
-		BackQuote = 96,
-		A = 97,
-		B = 98,
-		C = 99,
-		D = 100,
-		E = 101,
-		F = 102,
-		G = 103,
-		H = 104,
-		I = 105,
-		J = 106,
-		K = 107,
-		L = 108,
-		M = 109,
-		N = 110,
-		O = 111,
-		P = 112,
-		Q = 113,
-		R = 114,
-		S = 115,
-		T = 116,
-		U = 117,
-		V = 118,
-		W = 119,
-		X = 120,
-		Y = 121,
-		Z = 122,
-		Delete = 127,
-		Keypad0 = 256,
-		Keypad1 = 257,
-		Keypad2 = 258,
-		Keypad3 = 259,
-		Keypad4 = 260,
-		Keypad5 = 261,
-		Keypad6 = 262,
-		Keypad7 = 263,
-		Keypad8 = 264,
-		Keypad9 = 265,
-		KeypadPeriod = 266,
-		KeypadDivide = 267,
-		KeypadMultiply = 268,
-		KeypadMinus = 269,
-		KeypadPlus = 270,
-		KeypadEnter = 271,
-		KeypadEquals = 272,
-		UpArrow = 273,
-		DownArrow = 274,
-		RightArrow = 275,
-		LeftArrow = 276,
-		Insert = 277,
-		Home = 278,
-		End = 279,
-		PageUp = 280,
-		PageDown = 281,
-		F1 = 282,
-		F2 = 283,
-		F3 = 284,
-		F4 = 285,
-		F5 = 286,
-		F6 = 287,
-		F7 = 288,
-		F8 = 289,
-		F9 = 290,
-		F10 = 291,
-		F11 = 292,
-		F12 = 293,
-		F13 = 294,
-		F14 = 295,
-		F15 = 296,
-		Numlock = 300,
-		CapsLock = 301,
-		ScrollLock = 302,
-		RightShift = 303,
-		LeftShift = 304,
-		RightControl = 305,
-		LeftControl = 306,
-		RightAlt = 307,
-		LeftAlt = 308,
-		RightApple = 309,
-		RightCommand = 309,
-		LeftApple = 310,
-		LeftCommand = 310,
-		LeftWindows = 311,
-		RightWindows = 312,
-		AltGr = 313,
-		Help = 315,
-		Print = 316,
-		SysReq = 317,
-		Break = 318,
-		Menu = 319,
-		Mouse0 = 323,
-		Mouse1 = 324,
-		Mouse2 = 325,
-		Mouse3 = 326,
-		Mouse4 = 327,
-		Mouse5 = 328,
-		Mouse6 = 329,
-		JoystickButton0 = 330,
-		JoystickButton1 = 331,
-		JoystickButton2 = 332,
-		JoystickButton3 = 333,
-		JoystickButton4 = 334,
-		JoystickButton5 = 335,
-		JoystickButton6 = 336,
-		JoystickButton7 = 337,
-		JoystickButton8 = 338,
-		JoystickButton9 = 339,
-		JoystickButton10 = 340,
-		JoystickButton11 = 341,
-		JoystickButton12 = 342,
-		JoystickButton13 = 343,
-		JoystickButton14 = 344,
-		JoystickButton15 = 345,
-		JoystickButton16 = 346,
-		JoystickButton17 = 347,
-		JoystickButton18 = 348,
-		JoystickButton19 = 349,
-		Joystick1Button0 = 350,
-		Joystick1Button1 = 351,
-		Joystick1Button2 = 352,
-		Joystick1Button3 = 353,
-		Joystick1Button4 = 354,
-		Joystick1Button5 = 355,
-		Joystick1Button6 = 356,
-		Joystick1Button7 = 357,
-		Joystick1Button8 = 358,
-		Joystick1Button9 = 359,
-		Joystick1Button10 = 360,
-		Joystick1Button11 = 361,
-		Joystick1Button12 = 362,
-		Joystick1Button13 = 363,
-		Joystick1Button14 = 364,
-		Joystick1Button15 = 365,
-		Joystick1Button16 = 366,
-		Joystick1Button17 = 367,
-		Joystick1Button18 = 368,
-		Joystick1Button19 = 369,
-		Joystick2Button0 = 370,
-		Joystick2Button1 = 371,
-		Joystick2Button2 = 372,
-		Joystick2Button3 = 373,
-		Joystick2Button4 = 374,
-		Joystick2Button5 = 375,
-		Joystick2Button6 = 376,
-		Joystick2Button7 = 377,
-		Joystick2Button8 = 378,
-		Joystick2Button9 = 379,
-		Joystick2Button10 = 380,
-		Joystick2Button11 = 381,
-		Joystick2Button12 = 382,
-		Joystick2Button13 = 383,
-		Joystick2Button14 = 384,
-		Joystick2Button15 = 385,
-		Joystick2Button16 = 386,
-		Joystick2Button17 = 387,
-		Joystick2Button18 = 388,
-		Joystick2Button19 = 389,
-		Joystick3Button0 = 390,
-		Joystick3Button1 = 391,
-		Joystick3Button2 = 392,
-		Joystick3Button3 = 393,
-		Joystick3Button4 = 394,
-		Joystick3Button5 = 395,
-		Joystick3Button6 = 396,
-		Joystick3Button7 = 397,
-		Joystick3Button8 = 398,
-		Joystick3Button9 = 399,
-		Joystick3Button10 = 400,
-		Joystick3Button11 = 401,
-		Joystick3Button12 = 402,
-		Joystick3Button13 = 403,
-		Joystick3Button14 = 404,
-		Joystick3Button15 = 405,
-		Joystick3Button16 = 406,
-		Joystick3Button17 = 407,
-		Joystick3Button18 = 408,
-		Joystick3Button19 = 409,
-		Joystick4Button0 = 410,
-		Joystick4Button1 = 411,
-		Joystick4Button2 = 412,
-		Joystick4Button3 = 413,
-		Joystick4Button4 = 414,
-		Joystick4Button5 = 415,
-		Joystick4Button6 = 416,
-		Joystick4Button7 = 417,
-		Joystick4Button8 = 418,
-		Joystick4Button9 = 419,
-		Joystick4Button10 = 420,
-		Joystick4Button11 = 421,
-		Joystick4Button12 = 422,
-		Joystick4Button13 = 423,
-		Joystick4Button14 = 424,
-		Joystick4Button15 = 425,
-		Joystick4Button16 = 426,
-		Joystick4Button17 = 427,
-		Joystick4Button18 = 428,
-		Joystick4Button19 = 429,
-
 		// Cross buttons
 		Button1,
 		Button2,
 		Button3,
 		Button4,
+
+		// DPad buttons
+		DPadLeft,
+		DPadRight,
+		DPadDown,
+		DPadUp,
 
 		// Bumper buttons
 		BumperLeft,
@@ -274,6 +38,19 @@ namespace Reign
 		AxisRightY,
 		TriggerLeft,
 		TriggerRight
+	}
+
+	public enum ControllerPlayers
+	{
+		All,
+		Player1,
+		Player2,
+		Player3,
+		Player4,
+		//Player5,
+		//Player6,
+		//Player7,
+		//Player8
 	}
 
 	public enum ControllerTargets
@@ -308,41 +85,41 @@ namespace Reign
 			for (int i = 0; i != 6; ++i)
 			{
 				var analog = (AnalogTypes)i;
-				float value = GetAxis(analog);
+				float value = GetAxis(analog, ControllerPlayers.All);
 				if (value != 0) Debug.Log(string.Format("AnalogType {0} value: {1}", analog, value));
 			}
 		}
 
-		public static string GetPlatformButtonName(KeyCodeEx key)
+		public static string GetPlatformButtonName(Buttons key)
 		{
 			// Cross buttons
-			if (key == KeyCodeEx.Button1)
+			if (key == Buttons.Button1)
 			{
 				if (ControllerTarget == ControllerTargets.Xbox360) return "A";
 				else if (ControllerTarget == ControllerTargets.PS3) return "X";
 			}
-			else if (key == KeyCodeEx.Button2)
+			else if (key == Buttons.Button2)
 			{
 				if (ControllerTarget == ControllerTargets.Xbox360) return "B";
 				else if (ControllerTarget == ControllerTargets.PS3) return "Circle";
 			}
-			else if (key == KeyCodeEx.Button3)
+			else if (key == Buttons.Button3)
 			{
 				if (ControllerTarget == ControllerTargets.Xbox360) return "X";
 				else if (ControllerTarget == ControllerTargets.PS3) return "Square";
 			}
-			else if (key == KeyCodeEx.Button4)
+			else if (key == Buttons.Button4)
 			{
 				if (ControllerTarget == ControllerTargets.Xbox360) return "Y";
 				else if (ControllerTarget == ControllerTargets.PS3) return "Triangle";
 			}
 
 			// System buttons
-			else if (key == KeyCodeEx.Start)
+			else if (key == Buttons.Start)
 			{
 				return key.ToString();
 			}
-			else if (key == KeyCodeEx.Back)
+			else if (key == Buttons.Back)
 			{
 				if (ControllerTarget == ControllerTargets.PS3) return "Select";
 			}
@@ -350,96 +127,125 @@ namespace Reign
 			return key.ToString();
 		}
 
-		public static KeyCode ConvertKeyCode(KeyCodeEx key)
+		public static KeyCode ConvertKeyCode(Buttons key, ControllerPlayers player)
 		{
 			// Cross buttons
-			if (key == KeyCodeEx.Button1)
+			if (key == Buttons.Button1)
 			{
 				if (ControllerTarget == ControllerTargets.Xbox360) return KeyCode.JoystickButton0;
 				else if (ControllerTarget == ControllerTargets.PS3) return KeyCode.JoystickButton0;
 			}
-			else if (key == KeyCodeEx.Button2)
+			else if (key == Buttons.Button2)
 			{
 				if (ControllerTarget == ControllerTargets.Xbox360) return KeyCode.JoystickButton1;
 				else if (ControllerTarget == ControllerTargets.PS3) return KeyCode.JoystickButton0;
 			}
-			else if (key == KeyCodeEx.Button3)
+			else if (key == Buttons.Button3)
 			{
 				if (ControllerTarget == ControllerTargets.Xbox360) return KeyCode.JoystickButton2;
 				else if (ControllerTarget == ControllerTargets.PS3) return KeyCode.JoystickButton0;
 			}
-			else if (key == KeyCodeEx.Button4)
+			else if (key == Buttons.Button4)
 			{
 				if (ControllerTarget == ControllerTargets.Xbox360) return KeyCode.JoystickButton3;
 				else if (ControllerTarget == ControllerTargets.PS3) return KeyCode.JoystickButton0;
 			}
 
+			// DPad buttons
+			else if (key == Buttons.DPadLeft)
+			{
+				if (ControllerTarget == ControllerTargets.Xbox360) return KeyCode.None;// TODO
+				else if (ControllerTarget == ControllerTargets.PS3) return KeyCode.None;
+			}
+			else if (key == Buttons.DPadRight)
+			{
+				if (ControllerTarget == ControllerTargets.Xbox360) return KeyCode.None;
+				else if (ControllerTarget == ControllerTargets.PS3) return KeyCode.None;
+			}
+			else if (key == Buttons.DPadDown)
+			{
+				if (ControllerTarget == ControllerTargets.Xbox360) return KeyCode.None;
+				else if (ControllerTarget == ControllerTargets.PS3) return KeyCode.None;
+			}
+			else if (key == Buttons.DPadUp)
+			{
+				if (ControllerTarget == ControllerTargets.Xbox360) return KeyCode.None;
+				else if (ControllerTarget == ControllerTargets.PS3) return KeyCode.None;
+			}
+
 			// Bumper buttons
-			else if (key == KeyCodeEx.BumperLeft)
+			else if (key == Buttons.BumperLeft)
 			{
 				if (ControllerTarget == ControllerTargets.Xbox360) return KeyCode.JoystickButton4;
 				else if (ControllerTarget == ControllerTargets.PS3) return KeyCode.JoystickButton0;
 			}
-			else if (key == KeyCodeEx.BumperRight)
+			else if (key == Buttons.BumperRight)
 			{
 				if (ControllerTarget == ControllerTargets.Xbox360) return KeyCode.JoystickButton5;
 				else if (ControllerTarget == ControllerTargets.PS3) return KeyCode.JoystickButton0;
 			}
 
 			// System buttons
-			else if (key == KeyCodeEx.Start)
+			else if (key == Buttons.Start)
 			{
 				if (ControllerTarget == ControllerTargets.Xbox360) return KeyCode.JoystickButton7;
 				else if (ControllerTarget == ControllerTargets.PS3) return KeyCode.JoystickButton0;
 			}
-			else if (key == KeyCodeEx.Back)
+			else if (key == Buttons.Back)
 			{
 				if (ControllerTarget == ControllerTargets.Xbox360) return KeyCode.JoystickButton6;
 				else if (ControllerTarget == ControllerTargets.PS3) return KeyCode.JoystickButton0;
 			}
 
 			// Analog buttons
-			else if (key == KeyCodeEx.AnalogLeft)
+			else if (key == Buttons.AnalogLeft)
 			{
 				if (ControllerTarget == ControllerTargets.Xbox360) return KeyCode.JoystickButton8;
 				else if (ControllerTarget == ControllerTargets.PS3) return KeyCode.JoystickButton0;
 			}
-			else if (key == KeyCodeEx.AnalogRight)
+			else if (key == Buttons.AnalogRight)
 			{
 				if (ControllerTarget == ControllerTargets.Xbox360) return KeyCode.JoystickButton9;
 				else if (ControllerTarget == ControllerTargets.PS3) return KeyCode.JoystickButton0;
 			}
 
-			return (KeyCode)key;
+			return KeyCode.None;
 		}
 
-		public static bool GetKey(KeyCodeEx key)
+		public static bool GetButton(Buttons key, ControllerPlayers player)
 		{
-			return Input.GetKey(ConvertKeyCode(key));
+			return Input.GetKey(ConvertKeyCode(key, player));
 		}
 
-		public static bool GetKeyDown(KeyCodeEx key)
+		public static bool GetButtonDown(Buttons key, ControllerPlayers player)
 		{
-			return Input.GetKeyDown(ConvertKeyCode(key));
+			return Input.GetKeyDown(ConvertKeyCode(key, player));
 		}
 
-		public static bool GetKeyUp(KeyCodeEx key)
+		public static bool GetButtonUp(Buttons key, ControllerPlayers player)
 		{
-			return Input.GetKeyUp(ConvertKeyCode(key));
+			return Input.GetKeyUp(ConvertKeyCode(key, player));
 		}
 
-		private static string getAnalogTypeName(AnalogTypes type)
+		private static string getAnalogTypeName(AnalogTypes type, ControllerPlayers player)
 		{
-			switch (type)
+			const string axisFormat = "Axis{0}_{1}";
+			if (ControllerTarget == ControllerTargets.Xbox360)
 			{
-				case AnalogTypes.AxisLeftX: return "AxisLeftX";
-				case AnalogTypes.AxisLeftY: return "AxisLeftY";
-				case AnalogTypes.AxisRightX: return "AxisRightX";
-				case AnalogTypes.AxisRightY: return "AxisRightY";
-				case AnalogTypes.TriggerLeft: return "TriggerLeft";
-				case AnalogTypes.TriggerRight: return "TriggerRight";
-				default: return "Unsuported AnalogType: " + type;
+				switch (type)
+				{
+					case AnalogTypes.AxisLeftX: return string.Format(axisFormat, 1, player);
+					case AnalogTypes.AxisLeftY: return string.Format(axisFormat, 2, player);
+					case AnalogTypes.AxisRightX: return string.Format(axisFormat, 4, player);
+					case AnalogTypes.AxisRightY: return string.Format(axisFormat, 5, player);
+					case AnalogTypes.TriggerLeft: return string.Format(axisFormat, 9, player);
+					case AnalogTypes.TriggerRight: return string.Format(axisFormat, 10, player);
+					default: return "Unsuported AnalogType: " + type;
+				}
 			}
+			
+			Debug.LogError("Unknown ControllerTarget: " + ControllerTarget);
+			return "Unknown";
 		}
 
 		private static float processAnalogValue(float value)
@@ -447,14 +253,14 @@ namespace Reign
 			return (Mathf.Abs(value) <= AnalogTolerance) ? 0 : value;
 		}
 
-		public static float GetAxis(AnalogTypes type)
+		public static float GetAxis(AnalogTypes type, ControllerPlayers player)
 		{
-			return processAnalogValue(Input.GetAxisRaw(getAnalogTypeName(type)));
+			return processAnalogValue(Input.GetAxisRaw(getAnalogTypeName(type, player)));
 		}
 
-		public static float GetAxisRaw(AnalogTypes type)
+		public static float GetAxisRaw(AnalogTypes type, ControllerPlayers player)
 		{
-			return Input.GetAxisRaw(getAnalogTypeName(type));
+			return Input.GetAxisRaw(getAnalogTypeName(type, player));
 		}
 	}
 }
