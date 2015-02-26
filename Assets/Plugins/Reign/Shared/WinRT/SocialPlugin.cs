467850e8c7cac2b9519da8c118c22532b8ae04db
Andrew <zezba9000@gmail.com>
2015-02-25 19:40:49 -0800

    Social image sharing now works on all WinRT platforms.

--- Assets/Plugins/Reign/Shared/WinRT/SocialPlugin.cs
+++ Assets/Plugins/Reign/Shared/WinRT/SocialPlugin.cs
@@ -8,11 +8,12 @@ using Microsoft.Xna.Framework.Media;
 using Microsoft.Xna.Framework.Media.PhoneExtensions;
 #endif
 
-#if UNITY_WP_8_1
+#if UNITY_METRO || UNITY_WP_8_1
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.DataTransfer;
 using Windows.Foundation;
 using Windows.Storage;
+using Windows.UI.Core;
 #endif
 
 namespace Reign.Plugin
@@ -21,10 +22,10 @@ namespace Reign.Plugin
 	{
 		public void Share(byte[] data, SocialShareTypes type)
 		{
-			StreamManager.SaveFile("SharedImage.png", data, FolderLocations.Pictures, imageSavedCallback);
+			StreamManager.SaveFile("SharedImage.png", data, FolderLocations.Storage, imageSavedCallback);
 		}
 
-		private void imageSavedCallback(bool succeeded)
+		private async void imageSavedCallback(bool succeeded)
 		{
 			#if WINDOWS_PHONE
 			string filename;
@@ -37,13 +38,18 @@ namespace Reign.Plugin
 			var shareMediaTask = new ShareMediaTask();
 			shareMediaTask.FilePath = filename;
 			shareMediaTask.Show();
-			#elif UNITY_WP_8_1
-			var dataTransferManager = DataTransferManager.GetForCurrentView();
-			dataTransferManager.DataRequested += new TypedEventHandler<DataTransferManager, DataRequestedEventArgs>(shareStorageItemsHandler);
+			#elif UNITY_METRO || UNITY_WP_8_1
+			await WinRTPlugin.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, delegate()
+			{
+				var dataTransferManager = DataTransferManager.GetForCurrentView();
+				dataTransferManager.DataRequested -= shareStorageItemsHandler;
+				dataTransferManager.DataRequested += shareStorageItemsHandler;
+				DataTransferManager.ShowShareUI();
+			});
 			#endif
 		}
 
-		#if UNITY_WP_8_1
+		#if UNITY_METRO || UNITY_WP_8_1
 		private async void shareStorageItemsHandler(DataTransferManager sender, DataRequestedEventArgs e)
 		{
 			DataRequest request = e.Request;
@@ -56,6 +62,10 @@ namespace Reign.Plugin
 				var storageItems = new List<IStorageItem>();
 				storageItems.Add(shareFile);
 				request.Data.SetStorageItems(storageItems);       
+			}
+			catch (Exception ex)
+			{
+				UnityEngine.Debug.LogError(ex.Message);
 			}
 			finally
 			{
