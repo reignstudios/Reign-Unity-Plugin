708cdef8da5b5973bf1dd81fea92f08991913a5b
Andrew Witte <andrewwitte@Andrews-Mac-mini.local>
2015-04-07 00:55:35 -0700

    Updated Samsung IAP API. Injected code directly into the Reign jar file.

--- Native/Reign.Android/Native/ReignNativeSamsung/gen/com/example/reignnativesamsung/R.java
+++ Native/Reign.Android/Native/ReignNativeSamsung/gen/com/example/reignnativesamsung/R.java
@@ -10,11 +10,39 @@ package com.example.reignnativesamsung;
 public final class R {
     public static final class attr {
     }
+    public static final class dimen {
+        public static int page_loading_textview_textsize=0x7f040000;
+    }
     public static final class drawable {
-        public static int ic_launcher=0x7f020000;
+        public static int dialog_full_holo_light=0x7f020000;
+        public static int progressbar_middle=0x7f020001;
+        public static int tw_widget_progressbar_effect_holo_light=0x7f020002;
+        public static int tw_widget_progressbar_holo_light=0x7f020003;
+    }
+    public static final class id {
+        public static int body=0x7f070000;
+        public static int message=0x7f070001;
+    }
+    public static final class layout {
+        public static int progress_dialog=0x7f030000;
     }
     public static final class string {
-        public static int app_name=0x7f030000;
+        public static int IDS_SAPPS_BODY_WAITING_ING=0x7f05000a;
+        public static int IDS_SAPPS_HEADER_ITEM_PURCHASED=0x7f05000f;
+        public static int IDS_SAPPS_POP_ALREADY_PURCHASED=0x7f05000c;
+        public static int IDS_SAPPS_POP_AN_ERROR_OCCURRED_WHILE_RESETTING_SAMSUNG_IN_APP_PURCHASE=0x7f050001;
+        public static int IDS_SAPPS_POP_AN_INVALID_VALUE_HAS_BEEN_PROVIDED_FOR_SAMSUNG_IN_APP_PURCHASE=0x7f050002;
+        public static int IDS_SAPPS_POP_AN_UNEXPECTED_ERROR_HAS_OCCURRED_SAMSUNG_ACCOUNT_AUTHENTICATION_HAS_BEEN_CANCELLED=0x7f050003;
+        public static int IDS_SAPPS_POP_A_NEW_VERSION_OF_SAMSUNG_IN_APP_PURCHASE_IS_AVAILABLE_UPDATE_Q=0x7f050009;
+        public static int IDS_SAPPS_POP_NETWORK_UNAVAILABLE=0x7f05000e;
+        public static int IDS_SAPPS_POP_PAYMENT_CANCELLED=0x7f05000d;
+        public static int IDS_SAPPS_POP_PRODUCT_DOES_NOT_EXIST_IN_THIS_STORE=0x7f05000b;
+        public static int IDS_SAPPS_POP_SAMSUNG_IN_APP_PURCHASE=0x7f050006;
+        public static int IDS_SAPPS_POP_SAMSUNG_IN_APP_PURCHASE_NEEDS_TO_BE_UPDATED=0x7f050007;
+        public static int IDS_SAPPS_POP_TO_PURCHASE_ITEMS_YOU_NEED_TO_INSTALL_SAMSUNG_IN_APP_PURCHASE_INSTALL_Q=0x7f050008;
+        public static int IDS_SAPPS_POP_UNKNOWN_ERROR_OCCURRED=0x7f050005;
+        public static int IDS_SAPPS_POP_YOUR_PURCHASE_VIA_SAMSUNG_IN_APP_PURCHASE_IS_INVALID_A_FAKE_APPLICATION_HAS_BEEN_DETECTED_CHECK_THE_APP_MSG=0x7f050004;
+        public static int dlg_msg_payment_success=0x7f050000;
     }
     public static final class style {
         /** 
@@ -38,10 +66,12 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static int AppBaseTheme=0x7f040000;
+        public static int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static int AppTheme=0x7f040001;
+        public static int AppTheme=0x7f060001;
+        public static int IapProgressBar=0x7f060002;
+        public static int Theme_Empty=0x7f060003;
     }
 }
